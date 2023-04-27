4bc4fc297e3c6508a7726e256ba440bca52da1fc
James Nottidge <freshice57@gmail.com>
2023-04-27 23:36:32 +0100

    AddSlide Action test

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
+import App from "./App";
 import { StateProvider } from "./StateContext";
 import "./index.css";
 
+import { slideReducer } from "./controllers/slideReducer";
+
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <StateProvider reducer={}>
+    <StateProvider reducer={slideReducer}>
       <App />
     </StateProvider>
   </React.StrictMode>
