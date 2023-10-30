209b4f73a6e1572249f9c820430208e1ec1140f1
James Nottidge <freshice57@gmail.com>
2023-10-30 20:27:47 +0100

    validation on presentation json

--- api/src/routes/presentation-router.ts
+++ api/src/routes/presentation-router.ts
@@ -1,5 +1,7 @@
 import { Router } from "express";
 import { createPresentation } from "../controllers/presentation-controller";
+import { body } from "express-validator";
+import { handleInputErrors } from "../middleware/errorMiddleware";
 
 const presentationRouter = Router();
 
@@ -7,6 +9,15 @@ presentationRouter.get("/", (req, res) => {
   res.send("JAYS ON MY FEET");
 });
 
-presentationRouter.post("/create", createPresentation);
+presentationRouter.post(
+  "/create",
+  body("title").isString().isLength({ min: 1 }),
+  body("description").isString().isLength({ min: 1 }),
+  body("is_public").isBoolean().optional(),
+  body("is_published").isBoolean().optional(),
+  handleInputErrors,
+
+  createPresentation
+);
 
 export default presentationRouter;
