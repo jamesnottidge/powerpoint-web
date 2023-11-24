16dd60fbef8e69a99c55c8c6e1992d8ea745b977
James Nottidge <freshice57@gmail.com>
2023-11-24 20:06:08 +0100

    slide model

---
+++ api/src/database/models/Slide.ts
@@ -0,0 +1,29 @@
+import { Sequelize, DataTypes } from "sequelize";
+
+export const Slide = (sequelize: Sequelize) => {
+  return sequelize.define(
+    "Slide",
+    {
+      slide_id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      subtitle: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+    },
+    {
+      timestamps: true,
+    }
+  );
+};
